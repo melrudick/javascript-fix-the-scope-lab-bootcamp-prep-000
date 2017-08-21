5fc81534b51e2104cc1aab4b975d8dc9bcccfa18
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-21 01:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ function add2(n) {
   const two = 2;
 }
 
-var funkyFunction = "FUNKY!";
-  return function() {
-    return funkyFunction
+var funkyFunction = "FUNKY!" {
+  return funkyFunction;
   }
 
 
