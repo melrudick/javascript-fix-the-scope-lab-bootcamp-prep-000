492ddd2bf0e4b2d0db45585e741223495172a427
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-21 01:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function add2(n) {
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-var = funkyFunction = "FUNKY!" {
+var funkyFunction = "FUNKY!" {
 return function() {
 
 }
