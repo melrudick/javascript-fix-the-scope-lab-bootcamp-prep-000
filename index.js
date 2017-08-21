a83a98f5956dcc7dc556b91e30f104731fd7200e
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-21 02:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,8 @@ var funkyFunction = function() {
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
 
-var funkyFunction = 'FUNKY!' {
+var theFunk = funkyFunction()(){
 
-function funkyFunction(){
 
   return function()
 }
